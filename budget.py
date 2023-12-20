d2976656ce89bac2dd05d9e87af75b9898600616
jose zambrano <jose.zambrano@sinapsiscorp.com>
2023-12-20 12:08:41 -0500

    first commit

--- budget.py
+++ budget.py
@@ -1,7 +1,22 @@
 class Category:
+    def __init__(self,name):
+        self.name = name
+        self.ledger = []
 
+    def deposit(self,amount,description):
+        pass
+    
+    def withdraw(self,amount,description):
+        pass
+    
+    def get_balance(self):
+        pass
 
+    def transfer(self,amount,description):
+        return True
 
-
+    def check_funds(self):
+        pass
 
 def create_spend_chart(categories):
+    return "hello world"
